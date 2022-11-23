6b6ff8141c4177315c4dfc4fabc7ded3a61cf1e1
Andrey <supersoroko@gmail.com>
2022-11-23 21:36:06 +0300

    init: start React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./log.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img className="logo" src={logo} alt="Logo.png" />
       </header>
+      <nav className="nav">
+        <div>
+          <div>
+            <a>Profile</a>
+          </div>
+          <div>
+            <a>Messages</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Settings</a>
+          </div>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            className="friends"
+            src="https://www.maitre-mouhou.com/wp-content/uploads/2013/07/temoignage.jpg"
+          />
+        </div>
+        <div>
+          <img
+            className="ava"
+            src="https://www.armrus.org/upload/main/581/581c1a044fcd9f526747749f6eb7b93d.jpg"
+          />
+          Discription
+        </div>
+        <div>
+          My post
+          <div>New post</div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
